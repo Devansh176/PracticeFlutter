1db4a7bd3675c8b9e0d9d0fe3412baec1f8d3ed8
Devansh <devanshdhopte@gmail.com>
2024-10-09 12:23:40 +0530

    new directory and dart file moved there

---
+++ lib/CloneUIOne/CloneUIOne.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CloneOne extends StatelessWidget {
+  const CloneOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_outlined),
+
+      ),
+    );
+  }
+}
